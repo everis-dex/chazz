b6c7ad443102afb135e1340c36bad090b4028582
Angel Lalana <angeljavier.lalanacubelos@everis.nttdata.com>
2022-11-30 09:56:03 +0100

    blank react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        CHAZZ
+      </p>
+    </div>
+  );
+}
+
+export default App;
