640477cd3435a87e58af7c6c023a599c4f0052ad
Rocio Fernandez <rferferr@nttdata.com>
2022-11-30 13:07:25 +0100

    first commit (structure)

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    
+        return (
+            <>
+                <div className='menu'>
+                    adf
+                </div>
+            </>
+        )
+};
+
+export default Header;
