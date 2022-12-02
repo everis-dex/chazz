304fc2138b648edd6e0bea5bdc12204fa2d857ce
adri-st <abasteru@emeal.nttdata.com>
2022-12-02 11:34:10 +0100

    disable anchor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import './Header.scss'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Header.scss";
 
 const Header = () => {
-    
-        return (
-            <>
-                <div className='chazz-header'>
-                    <div className='velo'> 
-                        <div>
-                            <img src='./Logo_Chazz.png' alt='logo' className='chazz-logo'/>
-                        </div>
-                        <div className='nav'>
-                            <ul>
-                                <li>
-                                    <a href='#'>We are</a>
-                                </li>
-                                <li>
-                                    <a href='#'>Services</a>
-                                </li>
-                                <li>
-                                    <a href='#'>Work</a>
-                                </li>
-                                <li>
-                                    <a href='#'>Thoughts</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className='chazz-title'>
-                    <h1>Making the intangible, tangible</h1>
-                    <h4>Hybrid & Strategic Digital Agency</h4>
-                </div>
+  return (
+    <>
+      <div className="chazz-header">
+        <div className="velo">
+          <div>
+            <img src="./Logo_Chazz.png" alt="logo" className="chazz-logo" />
+          </div>
+          <div className="nav">
+            <ul>
+              <li>
+                <a href="#">We are</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Work</a>
+              </li>
+              <li>
+                <a href="#">Thoughts</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="chazz-title">
+        <h1>Making the intangible, tangible</h1>
+        <h4>Hybrid & Strategic Digital Agency</h4>
+      </div>
 
-                <div className='chazz-cookies'>
-                    <p>
-                        <strong>We use cookies to improve your browsing experience. </strong>
-                        If you want to know more, read more in our <a>Privacy Policy</a> and <a>Cookie Policy</a>.
-                    </p>
-                    <button>
-                        Allow cookies
-                    </button>
-                </div>
-            </>
-        )
+      <div className="chazz-cookies">
+        <p>
+          <strong>We use cookies to improve your browsing experience. </strong>
+          If you want to know more, read more in our <a>
+            Privacy Policy
+          </a> and <a>Cookie Policy</a>.
+        </p>
+        <button>Allow cookies</button>
+      </div>
+    </>
+  );
 };
 
 export default Header;
