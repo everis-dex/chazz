a661f03762e56b67095ef0171b3c6b63d28bb54d
adri-st <abasteru@emeal.nttdata.com>
2022-12-02 11:43:16 +0100

    change backend to git-gateway

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import PostList from "./components/postlist";
 
 export const App = () => {
   return (
     <>
       <Header />
-      <PostList />
     </>
   );
 };
