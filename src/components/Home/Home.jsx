32e7a8f234b6909e77a41a748ee9570856ba99ca
Rocio Fernandez <rferferr@nttdata.com>
2022-12-05 13:14:38 +0100

    feat: weare section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 import Header from "./Header/Header";
-
-
+import WeAre from "../Sections/WeAre/WeAre";
+import OurServices from "../Sections/OurServices/OurServices";
+import OurWork from "../Sections/OurWork/OurWork";
+import Thoughts from "../Sections/Thoughts/Thoughts";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <WeAre />
+      {/* <OurServices />
+      <OurWork />
+      <Thoughts /> */}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import "typeface-raleway";
 import App from "./App";
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <App />,
