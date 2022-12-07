26322a618358f5242a89b094537f7c523edd2ec7
adri-st <abasteru@emeal.nttdata.com>
2022-12-07 16:26:36 +0100

    fix: warnings

--- src/components/Home/HomeSections/HomeSection3/HomeSection3.jsx
+++ src/components/Home/HomeSections/HomeSection3/HomeSection3.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-import ChazzCarousel from "../../../shared/ChazzCarousel/ChazzCarousel";
-
-
+// import { Link } from "react-router-dom";
+// import ChazzCarousel from "../../../shared/ChazzCarousel/ChazzCarousel";
 
 const HomeSection3 = () => {
-
   return (
     <>
       <h3>Partners in crime</h3>
