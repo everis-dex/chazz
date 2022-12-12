5f7bfeab83899b057bf88afeeb11f3b36a62d9d9
vbeldaru <vbeldaru@emeal.nttdata.com>
2022-12-12 12:45:21 +0100

    feat: changed icon menu

--- src/components/Home/HomeHeader/HomeHeader.jsx
+++ src/components/Home/HomeHeader/HomeHeader.jsx
@@ -22,38 +22,25 @@ const Header = () => {
             <ul>
               <li>
                 <a href="/#HomeSection1">We are</a>{" "}
-                {/* Si desde el menú vamos al anchor #weare */}
               </li>
               <li>
                 <Link to={"/services"}>Services</Link>{" "}
-                {/* Si desde el menú vamos al anchor #services */}
               </li>
               <li>
                 <a href="/#HomeSection2">Work</a>{" "}
-                {/* Si desde el menú vamos al anchor #work */}
               </li>
               <li>
                 <Link to={"thoughts"}>Thoughts</Link>{" "}
-                {/* Si desde el menú vamos al anchor #thoughts */}
               </li>
             </ul>
           </div>
           <div className={`burger-menu ${menu ? "active" : ""}`}>
-            <button className="burger-button" onClick={toggleMenu}>
-              <svg
-                className="burger-svg"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                />
-              </svg>
-            </button>
+            <input id="open-close" name="open-close" type="checkbox" value="" />
+            <label
+              for="open-close"
+              class="toggle-button"
+              onClick={toggleMenu}
+            ></label>
             <nav className={`burger-nav ${menu ? "active" : ""}`}>
               <ul className="burger-ul">
                 <li className="burger-li">
