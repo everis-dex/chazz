e19004f4cd9d8d8c4b5e03f6dd998249cb5af74b
Rocio Fernandez <rferferr@nttdata.com>
2022-12-14 11:08:19 +0100

    fix styles

--- src/components/Home/HomeSections/HomeSection2/HomeSection2.tsx
+++ src/components/Home/HomeSections/HomeSection2/HomeSection2.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
-
 import { ChazzCarousel } from "./ChazzCarousel/ChazzCarousel";
-
 import { Container } from "react-bootstrap";
-
-import './HomeSection2.scss';
+import "./HomeSection2.scss";
 
 // import { Project } from "../../../../interfaces/interfaces";
-
-
 
 export const HomeSection2 = () => {
   return (
     <Container id="HomeSection2">
       <ChazzCarousel />
-      <Link to={"/work"} className="more-projects">More projects</Link>
+      <Link to={"/work"} className="more-projects">
+        More projects
+      </Link>
     </Container>
   );
 };
