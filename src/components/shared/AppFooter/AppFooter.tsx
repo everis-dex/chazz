1717996a9445d8beca63c31e9994045151c0c006
adri-st <abasteru@emeal.nttdata.com>
2022-12-15 17:01:06 +0100

    feat: AppFooter

--- src/components/shared/AppFooter/AppFooter.tsx
+++ src/components/shared/AppFooter/AppFooter.tsx
@@ -1,39 +1,25 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import './AppFooter.scss'
+
+import "./AppFooter.styles.scss";
 
 export const AppFooter = () => {
   return (
-    <>
-      <footer>
-        <Container>
-          <Row>
-            <Col className="section1-title" xs={12} sm={4} lg={4}>
-            <h6>an NTT Data Company</h6>
-            </Col>
-            <Col>
-              <Row>
-                <Col xs={12} sm={6} lg={3}>
-                  <a>©Chazz 2022</a>
-                </Col>
-
-                <Col xs={12} sm={6} lg={3}>
-                  <a>Privacy Policy</a>
-                </Col>
-
-                <Col xs={12} sm={6} lg={3}>
-                  <a>Cookie Policy</a>
-                </Col>
-
-                <Col xs={12} sm={6} lg={3}>
-                  <a>Cookie Settings</a> 
-                </Col>
-              </Row>
-              
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    </>
+    <footer>
+      <div className="footer">
+        <div className="footer-item">an NTT Data Company</div>
+        <div className="footer-item">
+          <div className="item">© Chazz 2022</div>
+          <a className="item" href="/#">
+            Privacy Policy
+          </a>
+          <a className="item" href="/#">
+            Cookie Policy
+          </a>
+          <a className="item" href="/#">
+            Cookie Settings
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
