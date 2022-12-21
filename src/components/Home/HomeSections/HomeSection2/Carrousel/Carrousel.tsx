cc47f76618d8a01c54fb7f059a5ee4604bd088a6
Angel Lalana <angeljavier.lalanacubelos@everis.nttdata.com>
2022-12-21 15:30:50 +0100

    expanding carousel to window-width but aligning first project card to the left of the title Featured projects

--- src/components/Home/HomeSections/HomeSection2/Carrousel/Carrousel.tsx
+++ src/components/Home/HomeSections/HomeSection2/Carrousel/Carrousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Autoplay, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -15,19 +15,60 @@ import "./Carrousel.styles.scss";
 export const Carrousel = () => {
   const slides: Project[] = [...projects];
 
+  const [carouselWidth, setCarouselWidth] = useState<number>(0);
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [offset, setOffset] = useState<number>(0);
+
+  const carouselRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const carouselWidth: number = (carouselRef != null && carouselRef.current)
+      ? carouselRef.current.offsetWidth
+      : 0;
+    setCarouselWidth(carouselWidth);
+
+    setOffset((windowWidth - carouselWidth) / 2 - 20);
+  }, []);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleWindowResize);
+
+    setOffset(windowWidth - carouselWidth - 20);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
+  useEffect(() => {
+    const carouselWidth: number = (carouselRef != null && carouselRef.current)
+      ? carouselRef.current.offsetWidth
+      : 0;
+    setCarouselWidth(carouselWidth);
+
+    setOffset(((windowWidth - carouselWidth) / 2) - 14);
+  }, [windowWidth]);
+
+
+
   return (
     <>
-      <div className="carouselTitle">
+      <div ref={carouselRef} className="carouselTitle">
         <h3>Featured projects</h3>
       </div>
 
-      <div className="carousel">
+      <div id="carousel" className="carousel">
         <div className="pagination" />
         <div className="slides">
           <Swiper
             modules={[Pagination, Autoplay]}
             slidesPerView={2.1}
-            spaceBetween={22}
+            spaceBetween={18}
             navigation
             pagination={{
               el: ".pagination",
@@ -43,10 +84,13 @@ export const Carrousel = () => {
               1024: { slidesPerView: 3.1 },
               1440: { slidesPerView: 2.1 }
             }}
+            initialSlide={0}
+            slidesOffsetBefore={offset}
           >
             {slides.map((slide: Project, index: number) => {
               return (
                 <SwiperSlide key={index}>
+                  {windowWidth} {carouselWidth} {offset}
                   <CarrouselSlide {...slide} />
                 </SwiperSlide>
               );
