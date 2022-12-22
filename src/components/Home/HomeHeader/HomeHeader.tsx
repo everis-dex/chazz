860a8f0a40c7eef6f039b13582dd43728089c20f
adri-st <abasteru@emeal.nttdata.com>
2022-12-22 09:44:20 +0100

    fix: change home header text & style

--- src/components/Home/HomeHeader/HomeHeader.tsx
+++ src/components/Home/HomeHeader/HomeHeader.tsx
@@ -15,7 +15,7 @@ export const HomeHeader = () => {
       </div>
 
       <div className="chazz-title">
-        <h1>Making the intangible, tangible</h1>
+        <h1>Empowering brands for real impact.</h1>
         <h4>Hybrid & Strategic Digital Agency</h4>
       </div>
 
