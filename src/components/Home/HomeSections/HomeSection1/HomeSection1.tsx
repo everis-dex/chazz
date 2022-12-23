c261144c6424cd8ba0487c6b1f2f70fc05caeaa7
Rocio Fernandez <rferferr@nttdata.com>
2022-12-23 09:04:31 +0100

    fix: resolve style fixes

--- src/components/Home/HomeSections/HomeSection1/HomeSection1.tsx
+++ src/components/Home/HomeSections/HomeSection1/HomeSection1.tsx
@@ -7,13 +7,17 @@ import { Col, Container, Row } from "react-bootstrap";
 import "./Section1.scss";
 
 export const HomeSection1 = () => {
+  const style = { fontSize: "40px" };
   return (
     <>
       <Container id="HomeSection1">
         <Row className="section1">
           <Col className="section1-title" xs={12} sm={6}>
+            <h2 className="strong">
+              {" "}
+              <strong>We humanize complexity. </strong>
+            </h2>
             <h2>
-              <strong>We humanize complexity. </strong> <br></br>
               Strategy, technology and creativity are our core. Building bridges to link with brands in a society
               centric focused way.
             </h2>
@@ -22,7 +26,7 @@ export const HomeSection1 = () => {
           <Col className="section1-sections" xs={12} sm={6}>
             {categories.map((category: Category, index) => (
               <div key={index}>
-                {category.title && <h2>{category.title}</h2>}
+                {category.title && <h2 style={style}>{category.title}</h2>}
                 {category.section && <p>{category.section}</p>}
                 {category.body && <p>{category.body}</p>}
                 <hr></hr>
