804c0161cecbdbdf8359a3143e7b963e673cea21
adri-st <abasteru@emeal.nttdata.com>
2023-01-04 15:19:45 +0100

    fix: add more studios

--- src/components/shared/Footer/CompanyInfo/CompanyInfoSections/Studios.tsx
+++ src/components/shared/Footer/CompanyInfo/CompanyInfoSections/Studios.tsx
@@ -8,10 +8,14 @@ import "../CompanyInfo.styles.scss";
 type Props = { title: string };
 
 export const Studios = ({ title }: Props) => {
+  const gridColumns = window.innerWidth > 480 ? 3 : 2;
+  // Define the number of rows so that there's always the required amount of columns (gridColumns)
+  const gridRows: number = Math.round(studios.length / gridColumns) + 1;
+
   return (
     <div className="studios-container">
       <p className="section-title">{title}</p>
-      <div className="studios-grid">
+      <div className="studios-grid" style={{ gridTemplateRows: `repeat(${gridRows}, 1fr)` }}>
         {studios.map((studio, index: number) => (
           <div className="studios-info" key={index}>
             <p className="city">{studio.city}</p>
