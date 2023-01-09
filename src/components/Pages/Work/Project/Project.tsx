c87f04b016ece49edbc4e692f3603b63fd8ced99
Angel Lalana <angeljavier.lalanacubelos@everis.nttdata.com>
2023-01-09 13:11:53 +0100

    fix first character issue in project body

--- src/components/Pages/Work/Project/Project.tsx
+++ src/components/Pages/Work/Project/Project.tsx
@@ -10,7 +10,12 @@ type Props = { data: IProject; columns?: string; height?: string; full?: boolean
 
 export const ProjectCard = ({ data, height = "auto", columns }: Props) => {
   const style = { width: "100%", height: height, objectFit: "cover" };
-  const bodyParagraphs: string = data.body.substring(2);
+
+  const bodyParagraphs = data.body.substring(1);
+
+  const bodyParagraphs1: string = bodyParagraphs.charCodeAt(0) === 10 ? bodyParagraphs.substring(1) : bodyParagraphs;
+  const bodyParagraphs2: string = bodyParagraphs1.charCodeAt(0) === 13 ? bodyParagraphs1.substring(1) : bodyParagraphs1;
+  const bodyParagraphs3: string = bodyParagraphs2.charCodeAt(0) === 8 ? bodyParagraphs2.substring(1) : bodyParagraphs2;
 
   // Dropdown
   const [openDropdown, setOpenDropdown] = useState<boolean>(false);
@@ -31,13 +36,13 @@ export const ProjectCard = ({ data, height = "auto", columns }: Props) => {
         <span className="properties">{data.subtitle}</span>
       </div>
 
-      <div className="non-accordion">{bodyParagraphs}</div>
+      <div className="non-accordion">{bodyParagraphs3}</div>
 
       <div className="project-dropdown">
         <Accordion onSelect={handleDropdown} flush>
           <Accordion.Item eventKey="0">
             <Accordion.Header>{dropdownText} information</Accordion.Header>
-            <Accordion.Body>{bodyParagraphs}</Accordion.Body>
+            <Accordion.Body>{bodyParagraphs3}</Accordion.Body>
           </Accordion.Item>
         </Accordion>
         <hr style={{ color: "red" }} />
