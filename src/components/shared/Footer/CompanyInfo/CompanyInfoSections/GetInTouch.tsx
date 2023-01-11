031004139bd2d110330e496a6756b7ecd77c62e2
Angel Lalana <angeljavier.lalanacubelos@everis.nttdata.com>
2023-01-11 10:59:24 +0100

    remove unused import

--- src/components/shared/Footer/CompanyInfo/CompanyInfoSections/GetInTouch.tsx
+++ src/components/shared/Footer/CompanyInfo/CompanyInfoSections/GetInTouch.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import { getInTouchInfo as getInTouch } from "../../../../../content/index";
 import { ISocial } from "../../../../../interfaces/cms";
