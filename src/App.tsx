83ad69a96818d77d0373630a33a1c5a7e08dce49
adri-st <abasteru@emeal.nttdata.com>
2023-01-11 15:24:54 +0100

    feat: hide services page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Home } from "./components/Home/Home";
-import { Services, Thoughts, Work } from "./components/Pages";
+import { /*Services,*/ Thoughts, Work } from "./components/Pages";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        {/* <Route path="/services" element={<Services />} /> */}
         <Route path="/work" element={<Work />} />
         <Route path="/thoughts" element={<Thoughts />} />
         <Route path="*" element={<Home />} />
