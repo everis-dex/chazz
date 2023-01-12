dd8c215bc61703e75a83c1f06e33214863bd8a3f
vbeldaru <vbeldaru@emeal.nttdata.com>
2023-01-12 13:48:05 +0100

    fix warning

--- src/components/shared/Nav/BurgerMenu/BurgerMenuOptions/BurgerMenuOptions.tsx
+++ src/components/shared/Nav/BurgerMenu/BurgerMenuOptions/BurgerMenuOptions.tsx
@@ -32,7 +32,7 @@ export const BurgerMenuOptions = ({ isBurgerMenuOpen, toggleMenu, disabledMenuOp
                 </Link>
               }
 
-              {(route.route == disabledMenuOption) &&
+              {(route.route === disabledMenuOption) &&
                 <span
                   className="burger-active"
                   // onClick={toggleMenu}
