e5be4787daaa5d2588efb49b2bbae8357a4cc72c
adri-st <abasteru@emeal.nttdata.com>
2023-01-20 11:43:44 +0100

    fix: projects

--- src/components/Home/HomeSections/2-projects/Projects.tsx
+++ src/components/Home/HomeSections/2-projects/Projects.tsx
@@ -1,32 +1,28 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { ReactComponent as RightArrow } from "../../../../assets/icon-right_arrow.svg";
+
 import { IHomeProjects } from "../../../../interfaces/cms";
 import { Carrousel } from "./Carrousel/Carrousel";
 
 import "./Projects.styles.scss";
 
-import rightArrow from "../../../../assets/icn_arrow.svg";
-import rightArrowHover from "../../../../assets/icn_arrow_hover.svg";
-
 export const Projects = (projectData: IHomeProjects) => {
   const [isHover, setIsHover] = useState<boolean>(false);
 
-  function handleHover(): void {
-    setIsHover(true);
-  }
-
-  function handleNoHover(): void {
-    setIsHover(false);
-  }
-
   return (
     <div className="background">
-      <div id="Projects">
+      <div className="projects">
         <Carrousel title={projectData.title} />
-        <Link to={"/work"} className="more-projects" onMouseEnter={handleHover} onMouseLeave={handleNoHover}>
-          {projectData.more}{" "}
-          <img src={!isHover ? rightArrow : rightArrowHover} className="icon-size" alt="right-arrow" />
+        <Link
+          to={"/work"}
+          className="more-projects"
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          {projectData.more}
+          <RightArrow stroke={!isHover ? "#191919" : "#fc82a3"} className="icon-size" />
         </Link>
       </div>
     </div>
