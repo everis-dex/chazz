96f135b54979d75c17ca5ec451c4aad4af7250c4
adri-st <abasteru@emeal.nttdata.com>
2023-01-20 14:33:48 +0100

    fix: remove Fragment

--- src/components/shared/Footer/CompanyInfo/CompanyInfoSections/GetInTouch.tsx
+++ src/components/shared/Footer/CompanyInfo/CompanyInfoSections/GetInTouch.tsx
@@ -10,19 +10,17 @@ import { RRSS } from "./RRSS";
 type Props = { title: string; socials: ISocial[] };
 export const GetInTouch = ({ title, socials }: Props) => {
   return (
-    <>
-      <div className="in-touch-container">
-        <p className="section-title">{title}</p>
-        <div className="in-touch-grid">
-          {getInTouch.map((item, index: number) => (
-            <div className="in-touch-info" key={index}>
-              <p className="in-touch-subtitle">{item.title}</p>
-              <LinkedEmail email={item.email} />
-            </div>
-          ))}
-          <RRSS showTitle={true} />
-        </div>
+    <div className="in-touch-container">
+      <p className="section-title">{title}</p>
+      <div className="in-touch-grid">
+        {getInTouch.map((item, index: number) => (
+          <div className="in-touch-info" key={index}>
+            <p className="in-touch-subtitle">{item.title}</p>
+            <LinkedEmail email={item.email} />
+          </div>
+        ))}
+        <RRSS showTitle={true} />
       </div>
-    </>
+    </div>
   );
 };
