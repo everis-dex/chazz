76192ad0933444f3d3ee0e96934707e8b86f716e
Angel Lalana <angeljavier.lalanacubelos@everis.nttdata.com>
2023-01-23 13:16:18 +0100

    fix some config errors

--- src/components/Pages/Work/Project/CaseStudy/CaseStudy.tsx
+++ src/components/Pages/Work/Project/CaseStudy/CaseStudy.tsx
@@ -1,11 +1,9 @@
-import { resolveObjectURL } from "buffer";
 import React from "react";
 
 import { projects } from "../../../../../content/index";
 import { IProject } from "../../../../../interfaces/cms";
 import { CaseInfoServices } from "./CaseInfo/CaseInfoServices";
 import { CaseInfoTitle } from "./CaseInfo/CaseInfoTitle";
-import { CaseInfoOneColumn } from "./Sections/CaseInfoOneColumn";
 
 // import "./CaseStudies.styles.scss";
 
@@ -13,7 +11,11 @@ console.log("PROJECTS", projects);
 type Props = { caseStudyId: number };
 
 export const CaseStudy = ({ caseStudyId }: Props) => {
-  const project: IProject = projects.filter(project => project.id === caseStudyId)[0] as IProject;
+  console.log({ projects });
+  console.log({ caseStudyId });
+  // const project: IProject = projects.filter(project => project.id === caseStudyId)[0] as IProject;
+  const project: IProject = projects.filter(project => project.id === caseStudyId)];
+  console.log({ project });
 
   return (
     <>
