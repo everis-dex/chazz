f4a4fe5043e1d4aff254bde2c8a09a5ea324c4a8
aarteagh <alejandro.arteagahernandez.st@emeal.nttdata.com>
2023-01-24 09:48:13 +0100

    Revert "cambios"
    This reverts commit c6975b7d646a4e1a6883d2d7111c4c25a462482a.

--- src/components/Pages/Work/Project/CaseStudy/CaseStudy.tsx
+++ src/components/Pages/Work/Project/CaseStudy/CaseStudy.tsx
@@ -14,7 +14,7 @@ export const CaseStudy = ({ caseStudyId }: Props) => {
   console.log({ caseStudyId });
   console.log({ projects });
   const project: IProject = projects.filter(project => project.id === caseStudyId)[0] as IProject;
-  console.log(project.sections.firstFWClaim);
+  console.log({ project });
 
   return (
     <>
