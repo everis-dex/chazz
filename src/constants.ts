e3fa0ca265921fc279dde43cc59d066e3f577f96
adri-st <abasteru@emeal.nttdata.com>
2023-02-02 13:45:32 +0100

    fix: hide services page

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const routesInfo: RoutesInfo[] = [
 ];
 
 // select here which nav options are available
-export const availableRouteIDs: number[] = [0, 1, 2];
+export const availableRouteIDs: number[] = [0, 2];
 
 export const desktopLineBreakSymbol: string = "+";
 export const mobileLineBreakSymbol: string = "*";
