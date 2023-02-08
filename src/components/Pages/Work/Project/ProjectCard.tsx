9da0177775a83a375d8db24216ea4d4194b29e2a
SergioSoteras <sergiosoteras@hotmail.com>
2023-02-08 13:08:29 +0100

    Links in Word to Cases disabled

--- src/components/Pages/Work/Project/ProjectCard.tsx
+++ src/components/Pages/Work/Project/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import { IProject } from "../../../../interfaces/cms";
 import { Accordion } from "../../../shared/Accordion/Accordion";
@@ -30,9 +29,8 @@ export const ProjectCard = ({ data, format, columns }: Props) => {
 
       <div className="project-details">
         <div className="project-title-container">
-          <Link className="title" to={`/work/case/${data.id}`}>
-            {data.title} —
-          </Link>
+          {/* Sustituir span de title por Link, y cambiar en styles el hover de la clase title */}
+          <span className="title">{data.title} —</span>
           <span className="description">{data.description}</span>
         </div>
         <span className="properties">{data.subtitle}</span>
