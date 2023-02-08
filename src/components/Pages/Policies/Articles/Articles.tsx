bef12a353b4d25834da2d70b9d29cc512f5fc81d
SergioSoteras <sergiosoteras@hotmail.com>
2023-02-08 17:42:39 +0100

    Accordion styles updated and useEffect import delered

--- src/components/Pages/Policies/Articles/Articles.tsx
+++ src/components/Pages/Policies/Articles/Articles.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { IPolicy, IPolicyArticle } from "../../../../interfaces/cms";
 import { Accordion } from "../../../shared/Accordion/Accordion";
