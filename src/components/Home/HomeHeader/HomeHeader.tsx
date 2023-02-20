dd0d1fda2c7a3fb6d9f45ad61785653bd14cb025
adri-st <abasteru@emeal.nttdata.com>
2023-02-20 11:33:20 +0100

    fix: home header animation

--- src/components/Home/HomeHeader/HomeHeader.tsx
+++ src/components/Home/HomeHeader/HomeHeader.tsx
@@ -69,16 +69,8 @@ export const HomeHeader = (headerData: IHomeHeader) => {
     };
   }, []);
 
-  function appHeight(): void {
-    const doc = document.documentElement;
-    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-  }
-  window.addEventListener("resize", appHeight);
-  appHeight();
-  //onWheel
-  const handleOnWheel: React.WheelEventHandler<HTMLDivElement> = e => {
-    if (e.deltaY > 0) {
-      //alert("XD");
+  useEffect(() => {
+    setTimeout(() => {
       if (titleLeft > 16) {
         if (navHeight > 10) setNavHeight(10);
         setTitleLeft(16);
@@ -86,15 +78,23 @@ export const HomeHeader = (headerData: IHomeHeader) => {
         setAnimationComplete(true);
         document.body.classList.remove("no-scroll");
       }
-    }
-  };
+    }, 1000);
+  }, []);
+
+  function appHeight(): void {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  }
+  window.addEventListener("resize", appHeight);
+  appHeight();
+
   useEffect(() => {
     // Agregar la clase al body para ocultar el overflow
     document.body.classList.add("no-scroll");
   }, []);
   return (
-    <div className="chazz-header" onWheel={handleOnWheel}>
-      <div className={isPlaying ? "velo-out" : "velo-in"} onWheel={handleOnWheel}>
+    <div className="chazz-header">
+      <div className={isPlaying ? "velo-out" : "velo-in"}>
         <div className={isPlaying ? "simply-out" : "simply-in"}>
           <span className={isPlaying ? "nav-out" : "nav-in"}>
             <Nav isPlaying={isPlaying} AlertNavParent={AlertNavParent} height={navHeight} />
--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -31,7 +31,7 @@ export const Nav = ({ isPlaying = false, darkMode = false, AlertNavParent, heigh
 
   return (
     <div className="header-nav" style={{ height: `${height}vh` }}>
-      <ChazzLogo color={logoColor} isBurgerMenuOpen={isBurgerMenuOpen} />
+      <ChazzLogo color={logoColor} />
       <DesktopMenu {...displayMode} />
       <BurgerMenu isBurgerMenuOpen={isBurgerMenuOpen} toggleMenu={toggleMenu} color={color} />
     </div>
