49cfe4fc91023be185906b4a4ea2be00a4c7055c
aarteagh0 <123374992+aarteagh0@users.noreply.github.com>
2023-02-21 16:40:36 +0100

    Work animation

--- src/components/shared/Footer/CompanyInfo/CompanyInfo.tsx
+++ src/components/shared/Footer/CompanyInfo/CompanyInfo.tsx
@@ -8,13 +8,13 @@ import "./CompanyInfo.styles.scss";
 
 export const CompanyInfo = (footerData: IHomeFooter) => {
   return (
-    <div className="we-are-section">
+    <div className="we-are-section" data-aos="fade-up" data-aos-once="true">
       <div className="we-are-grid">
         <h2 data-aos="fade-up" data-aos-once="true">
           <strong>{footerData.title}</strong>
         </h2>
         <Offices title={footerData.offices} />
-        <DividingLine  />
+        <DividingLine />
         <Studios title={footerData.studios} />
         <DividingLine />
         <GetInTouch title={footerData.touch} />
