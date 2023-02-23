68c8727069e16d73ad3915ffbcd4fad49c19b223
adri-st <abasteru@emeal.nttdata.com>
2023-02-23 11:14:56 +0100

    refactor AllowCookies

--- src/components/shared/AllowCookies/AllowCookies.tsx
+++ src/components/shared/AllowCookies/AllowCookies.tsx
@@ -6,24 +6,23 @@ export const AllowCookies = () => {
   const cookiesAllowed = localStorage.getItem("cookiesAllowed");
   const [displayCookies, setDisplayCookies] = useState<boolean>(cookiesAllowed ? false : true);
 
-  // TODO: To clean up cookies
-  // localStorage.removeItem("cookiesAllowed");
-
   function handleAllowCookies(): void {
     localStorage.setItem("cookiesAllowed", "true");
     setDisplayCookies(false);
   }
 
-  return cookiesAllowed && !displayCookies ? (
-    <></>
-  ) : (
-    <div className="chazz-cookies">
-      <p>
-        <strong>We use cookies to improve your browsing experience. </strong>
-        If you want to know more, read more in our &nbsp;
-        <a href="/policies/0">Privacy Policy</a> and <a href="/policies/1">Cookie Policy</a>.
-      </p>
-      <button onClick={handleAllowCookies}>Allow cookies</button>
-    </div>
+  return (
+    <>
+      {cookiesAllowed && !displayCookies && (
+        <div className="chazz-cookies">
+          <p>
+            <strong>We use cookies to improve your browsing experience. </strong>
+            If you want to know more, read more in our &nbsp;
+            <a href="/policies/0">Privacy Policy</a> and <a href="/policies/1">Cookie Policy</a>.
+          </p>
+          <button onClick={handleAllowCookies}>Allow cookies</button>
+        </div>
+      )}
+    </>
   );
 };
