a4c8515da2cc830b43495e9dc5c4a1d1b7e0cf5a
adri-st <abasteru@emeal.nttdata.com>
2023-02-24 10:36:13 +0100

    remove unused libraries

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "typeface-raleway";
 import "./index.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
