1b6ffe8a899b6890b6c7912325a2e167bd724051
SergioSoteras <sergiosoteras@hotmail.com>
2023-02-24 14:10:16 +0100

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,6 +4,7 @@ export function useScrollToTop(): void | undefined {
   useEffect(() => {
     document.body.classList.remove("no-scroll");
     setTimeout(() => {
+      //Sin Timeout hace el calculo antes de renderizar todos los componentes y falla el scrollTo
       window.scrollTo(0, 0);
     }, 200);
   }, []);
