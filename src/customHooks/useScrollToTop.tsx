644dc2780fc8f28a7f101927e6157b33b75dedc0
SergioSoteras <sergiosoteras@hotmail.com>
2023-02-28 18:25:06 +0100

    Update useScrollToTop.tsx

--- src/customHooks/useScrollToTop.tsx
+++ src/customHooks/useScrollToTop.tsx
@@ -5,14 +5,11 @@ export function useScrollToTop(): void | undefined {
   const location = useLocation();
   useEffect(() => {
     document.body.classList.remove("no-scroll");
-    const isHomePage = location.pathname === "/" || location.key === "default";
-    if (isHomePage) document.body.classList.add("no-scroll");
-    setTimeout(
-      () => {
-        //Sin Timeout hace el calculo antes de renderizar todos los componentes y falla el scrollTo
-        window.scrollTo(0, 0);
-      },
-      isHomePage ? 400 : 100
-    );
+    if (location.pathname === "/" || (location.key === "default" && !location.pathname.includes("policies")))
+      document.body.classList.add("no-scroll");
+    setTimeout(() => {
+      //Sin Timeout hace el calculo antes de renderizar todos los componentes y falla el scrollTo
+      window.scrollTo(0, 0);
+    }, 400);
   }, [location]);
 }
